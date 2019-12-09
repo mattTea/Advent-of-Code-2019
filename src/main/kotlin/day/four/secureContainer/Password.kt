dad65d1be4e7494acded74a6a36847671bb4e3e5
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-12-09 21:25:14 +0000

    Day 4: Part 2 complete

--- src/main/kotlin/day/four/secureContainer/Password.kt
+++ src/main/kotlin/day/four/secureContainer/Password.kt
@@ -2,14 +2,30 @@ package day.four.secureContainer
 
 val range = (356261..846303).toList()
 
-fun decodePassword(range: List<Int>): List<Int> {
+fun main() {
+    println(decodePassword(range).size)
+}
+
+internal fun decodePassword(range: List<Int>): List<Int> {
     return range.filter { code ->
         val numbers = code.toString().map { it.toString().toInt() }
 
-        numbers[0] == numbers[1] ||
-        numbers[1] == numbers[2] ||
-        numbers[2] == numbers[3] ||
-        numbers[3] == numbers[4] ||
-        numbers[4] == numbers[5]
+        equalNeighbours(numbers) && increasingDigits(numbers)
     }
+}
+
+private fun equalNeighbours(numbers: List<Int>): Boolean {
+    return ((numbers[0] == numbers[1]) && (numbers[1] != numbers[2])) ||
+            ((numbers[1] == numbers[2]) && (numbers[2] != numbers[3]) && (numbers[1] != numbers[0])) ||
+            ((numbers[2] == numbers[3]) && (numbers[3] != numbers[4]) && (numbers[2] != numbers[1])) ||
+            ((numbers[3] == numbers[4]) && (numbers[4] != numbers[5]) && (numbers[3] != numbers[2])) ||
+            ((numbers[4] == numbers[5]) && (numbers[3] != numbers[4]))
+}
+
+private fun increasingDigits(numbers: List<Int>): Boolean {
+    return numbers[0] <= numbers[1] &&
+            numbers[1] <= numbers[2] &&
+            numbers[2] <= numbers[3] &&
+            numbers[3] <= numbers[4] &&
+            numbers[4] <= numbers[5]
 }
--- src/test/kotlin/day/four/secureContainer/PasswordTest.kt
+++ src/test/kotlin/day/four/secureContainer/PasswordTest.kt
@@ -7,16 +7,23 @@ import org.spekframework.spek2.style.specification.describe
 
 object PasswordTest : Spek({
     describe("decrypt()") {
-        it("should return 6-digit values with equal adjacent digits") {
-            val testRange = (101010..101011).toList()
+        it("should return values with equal adjacent digits & digits increasing left to right") {
+            val testRange = (223454..223455).toList()
 
-            assertThat(decodePassword(testRange)).isEqualTo(listOf(101011))
+            assertThat(decodePassword(testRange)).isEqualTo(listOf(223455))
+        }
+
+        it("should not return values where equal adjacent digits are part of a larger group of equal digits") {
+            val testRange = (123444..123445).toList()
+
+            assertThat(decodePassword(testRange)).isEqualTo(listOf(123445))
         }
     }
 })
 
 /*
-
+PART 1
+------
 It is a six-digit number.
 The value is within the range given in your puzzle input. -> 356261-846303
 Two adjacent digits are the same (like 22 in 122345).
@@ -29,4 +36,11 @@ Other than the range rule, the following are true:
 How many different passwords within the range given in your puzzle input meet these criteria?
 
 
+PART 2
+------
+The two adjacent matching digits are not part of a larger group of matching digits
+
+112233 meets these criteria because the digits never decrease and all repeated digits are exactly two digits long.
+123444 no longer meets the criteria (the repeated 44 is part of a larger group of 444).
+111122 meets the criteria (even though 1 is repeated more than twice, it still contains a double 22)
 */
