2147f4f61f14dba783599cdd4f272f51412483c6
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-12-14 14:58:23 +0000

    Day 3: Record each point passed for single instructions

--- src/main/kotlin/day/three/wires/MeasureWires.kt
+++ src/main/kotlin/day/three/wires/MeasureWires.kt
@@ -2,10 +2,15 @@ package day.three.wires
 
 typealias Point = Pair<Int, Int>
 
-fun plotWire(instruction: String): Point {
+fun plotWire(instruction: String): List<Point> {
     val start = Point(0, 0)
+    val end = start.plot(calculatePoint(instruction))
 
-    return start.plot(calculatePoint(instruction))
+    val path = mutableListOf(Point(0, 0))
+
+    path += capturePath(instruction, start, end)
+
+    return path.distinct()
 }
 
 private fun Point.plot(end: Point): Point {
@@ -21,3 +26,57 @@ private fun calculatePoint(instruction: String): Point {
         else -> Point(0, 0)
     }
 }
+
+private fun capturePath(instruction: String, start: Pair<Int, Int>, end: Pair<Int, Int>): MutableList<Point> {
+    val pathPoints = mutableListOf(Point(0, 0))
+
+    when (instruction.first()) {
+        'R' -> pathPoints += rightPath(start, end)
+        'L' -> pathPoints += leftPath(start, end)
+        'U' -> pathPoints += upPath(start, end)
+        'D' -> pathPoints += downPath(start, end)
+    }
+
+    return pathPoints
+}
+
+private fun rightPath(start: Pair<Int, Int>, end: Pair<Int, Int>): List<Point> {
+    val path = mutableListOf(Point(0, 0))
+
+    for (coordinate in start.first..end.first) {
+        path.add(Point(coordinate, start.second))
+    }
+
+    return path.distinct()
+}
+
+private fun leftPath(start: Pair<Int, Int>, end: Pair<Int, Int>): List<Point> {
+    val path = mutableListOf(Point(0, 0))
+
+    for (coordinate in start.first downTo end.first) {
+        path.add(Point(coordinate, start.second))
+    }
+
+    return path.distinct()
+}
+
+private fun upPath(start: Pair<Int, Int>, end: Pair<Int, Int>): List<Point> {
+    val path = mutableListOf(Point(0, 0))
+
+    for (coordinate in start.second..end.second) {
+        path.add(Point(start.first, coordinate))
+    }
+
+    return path.distinct()
+}
+
+private fun downPath(start: Pair<Int, Int>, end: Pair<Int, Int>): List<Point> {
+    val path = mutableListOf(Point(0, 0))
+
+    for (coordinate in start.second downTo end.second) {
+        path.add(Point(start.first, coordinate))
+    }
+
+    return path.distinct()
+}
+
--- src/test/kotlin/day/three/wires/MeasureWiresTest.kt
+++ src/test/kotlin/day/three/wires/MeasureWiresTest.kt
@@ -1,6 +1,7 @@
 package day.three.wires
 
 import assertk.assertThat
+import assertk.assertions.contains
 import assertk.assertions.isEqualTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -8,19 +9,23 @@ import org.spekframework.spek2.style.specification.describe
 object MeasureWiresTest : Spek({
     describe("plotWire()") {
         it("should plot to a point using R instruction") {
-            assertThat(plotWire("R2")).isEqualTo(Point(2, 0))
+            assertThat(plotWire("R2")).contains(Point(2, 0))
         }
 
         it("should plot to a point using L instruction") {
-            assertThat(plotWire("L3")).isEqualTo(Point(-3, 0))
+            assertThat(plotWire("L3")).contains(Point(-3, 0))
         }
 
         it("should plot to a point using U instruction") {
-            assertThat(plotWire("U4")).isEqualTo(Point(0, 4))
+            assertThat(plotWire("U4")).contains(Point(0, 4))
         }
 
         it("should plot to a point using D instruction") {
-            assertThat(plotWire("D15")).isEqualTo(Point(0, -15))
+            assertThat(plotWire("D15")).contains(Point(0, -15))
+        }
+
+        it("should record the 2 points that plot passes through") {
+            assertThat(plotWire("R2")).isEqualTo(listOf(Point(0, 0), Point(1, 0), Point(2, 0)))
         }
     }
 })
@@ -30,6 +35,8 @@ object MeasureWiresTest : Spek({
 1. Add together 2 coordinates
 2. Draw line between 2 coordinates
     - Keep track of where started and where ended
+
+    - record every point it goes through
 
 3. Draw line between 3 coordinates
     - Keep track of where been?
